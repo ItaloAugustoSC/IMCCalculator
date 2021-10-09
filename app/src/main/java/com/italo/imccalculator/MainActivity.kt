ea989fff3cc361eef6e40b8ad9f8c860e20c287f
Ítalo Augusto <72274951+ItaloAugustoSC@users.noreply.github.com>
2021-10-08 23:42:19 -0300

    Added result feature.

--- app/src/main/java/com/italo/imccalculator/MainActivity.kt
+++ app/src/main/java/com/italo/imccalculator/MainActivity.kt
@@ -1,15 +1,44 @@
 package com.italo.imccalculator
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doAfterTextChanged
 import android.util.Log
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.w("lifecycle", "Entrei no CREATE")
+        //Log.w("lifecycle", "Entrei no CREATE")
+        setListeners()
+    }
 
+    private fun setListeners(){
+
+        alturaEDT?.doAfterTextChanged { text ->
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+        }
+
+        pesoEDT?.doOnTextChanged { text, _, _, _ ->
+            titleTXT?.text = text
+        }
+
+        resultadoBTN?.setOnClickListener{
+            IMCCalculus(pesoEDT.text.toString(), alturaEDT.text.toString())
+        }
+    }
+
+    private fun IMCCalculus(peso: String, altura: String) {
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if(peso != null && altura != null){
+            val imc = peso / (altura * altura)
+            titleTXT.text = "Seu IMC é:\n%.2f".format(imc)
+        }
     }
 
     override fun onStart() {
